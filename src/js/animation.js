d2e041bbf16d87eb229ff19475b595e1a7715f84
Hanna Forssell <forssellhanna@gmail.com>
2023-02-24 11:04:49 +0100

    add game over animations

--- src/js/animation.js
+++ src/js/animation.js
@@ -7,4 +7,20 @@ gsap.to('#alexandra', {
     transformOrigin: '60% center',
     repeat: -1,
     ease: 'none',
-  });
+});
+
+gsap.to('.game-over', {
+  rotation: 360, 
+  duration: 3, 
+  transformOrigin: 'center center', 
+  repeat: -1,
+  ease: 'none',
+});
+
+gsap.to('#restartGameBtn', {
+  rotation: 360, 
+  duration: 3, 
+  transformOrigin: 'center center', 
+  repeat: -1,
+  ease: 'none',
+})
